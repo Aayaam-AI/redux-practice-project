cf359453dc22ad92c5a11c13e647ef40cdb0f2c1
Aayaam-AI <aayaamagarwal@gmail.com>
2021-05-27 22:30:27 +0530

    update cart by fetching data from firebase

---
+++ src/store/cart-actions.js
@@ -0,0 +1,83 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-http-4bb0d-default-rtdb.firebaseio.com/cart.json"
+      );
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+      const data = await response.json();
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Failed to retrieve cart data",
+        })
+      );
+    }
+  };
+};
+
+// Here we created action creator.
+// This is called a 'thunk' which return a function that executes a
+// action after some amount of time. This is async behaviour.
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "sending",
+        message: "sending cart data",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-4bb0d-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "success!",
+          message: "sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Failed to send cart data",
+        })
+      );
+    }
+  };
+};
